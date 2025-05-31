fb23ed3212dfd088cc16e11dc50eed0115c813d1
alanuwu <nosaboxdxdjajasalu2@gmail.com>
2025-05-31 01:19:13 -0600

    feat: signin implementado

---
+++ app/signin/page.tsx
@@ -0,0 +1,10 @@
+
+import SignInForm from "./_components/SignInForm";
+
+export default function SignInPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+      <SignInForm />
+    </div>
+  );
+}
