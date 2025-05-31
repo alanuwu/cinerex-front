4a7d42b1ba7e52ad801f090543d5384772a4a7a6
AlejandroBR10 <alejabaldera@gmail.com>
2025-05-31 12:04:30 -0600

    Agregando todas las entidades

--- entities.ts
+++ entities.ts
@@ -1,2 +1,94 @@
 //AQUI VAN TODAS LAS ENTIDADES QUE TENEMOS EN BACK
 // A CONSIDERAR LO DE LOS ASIENTOS Y RESERVACIONES
+
+
+
+export interface User {
+  userId: string;
+
+
+  userEmail: string;
+
+
+  userPassword: string;
+
+userRoles: string[];
+
+}
+
+export interface Customer{
+    
+  customerId: string;
+
+  customerName: string;
+  customerLastName: string;
+  customerEmail: string;
+  customerPhoneNumber: string;
+
+  tickets: Ticket[];
+
+  user: User | string;
+}
+
+export interface Movie {
+      
+  movieId: number;
+
+  movieTitle: string;
+
+
+  movieDescription: string;
+
+
+  movieDurationMinutes: number;
+
+
+  movieGenre: string;
+
+
+  movieImageUrl: string;
+
+  showtimes: Showtime[];
+}
+
+export interface Room{
+
+  roomId: string;
+
+ 
+  roomName: string;
+
+ 
+  roomCapacity: number;
+
+
+  showtimes: Showtime[];
+}
+
+export interface Showtime{
+     id: string;
+
+  dateTime: Date;
+  
+  price: number;
+  remainingSeats: number;
+
+  movie: Movie | string;
+
+  room:   Room | string;
+
+  tickets: Ticket[];
+}
+
+export interface Ticket {
+      
+  id: string;
+  price: number;
+
+  purchaseDate: Date;
+
+  customer: Customer | string;
+
+  showtime: Showtime | string;
+
+}
